563c9dca2e656ca848d24c585810ce1d5b0e55a0
rainbow5079 <rlaxogh507906@gmail.com>
2024-04-02 18:13:16 +0900

    Add UUID Format Checker

---
+++ utility/checker.py
@@ -0,0 +1,6 @@
+import re
+
+def is_valid_format(data: str) -> bool:
+    UUID_PATTERN = re.compile(r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$")
+    
+    return bool(re.match(UUID_PATTERN, data))
