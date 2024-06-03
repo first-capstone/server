b393a66f2a10f41dcd0d7be3383923cc6ca6b032
rainbow5079 <rlaxogh507906@gmail.com>
2024-06-04 01:03:47 +0900

    :memo: FastAPI 기본 상태코드 422 ResponseModel 제거

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from models.response import ResponseModel, ResponseStatusCode
 from fastapi.exceptions import RequestValidationError
+from starlette.middleware.cors import CORSMiddleware
 from env.UNIVERSITY import CARRERNET_URL, API_KEY
+from fastapi.openapi.utils import get_openapi
 from database.conn import DBObject
 from models import University
 from fastapi import FastAPI
@@ -9,8 +11,40 @@ import routers
 
 
 app = FastAPI()
+
+def custom_openapi():
+    if not app.openapi_schema:
+        app.openapi_schema = get_openapi(
+            title=app.title,
+            version=app.version,
+            openapi_version=app.openapi_version,
+            description=app.description,
+            terms_of_service=app.terms_of_service,
+            contact=app.contact,
+            license_info=app.license_info,
+            routes=app.routes,
+            tags=app.openapi_tags,
+            servers=app.servers,
+        )
+        for _, method_item in app.openapi_schema.get('paths').items():
+            for _, param in method_item.items():
+                responses = param.get('responses')
+                if '422' in responses:
+                    del responses['422']
+
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 app.include_router(routers.univ_router)
 app.include_router(routers.account_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
